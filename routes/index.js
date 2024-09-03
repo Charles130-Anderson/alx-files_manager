95c773653c0e64dc6aeffee979067a92959b1f13
root <andersonoyoo034@gmail.com>
2024-09-03 01:37:27 -0700

    Add user creation with validation..

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
-// Existing routes
+// Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// New route for user creation
-router.post('/users', UsersController.postNew);
+router.post('/users', UserController.postNew);
+router.get('/users/me', UserController.getMe);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
